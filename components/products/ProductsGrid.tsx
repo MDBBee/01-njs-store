cfe6a7ac7928114508d79d3a243ecf67364dd3e6
MDBBee <bobbyugbebor@gmail.com>
2024-12-16 06:19:44 +0200

    .

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -12,6 +12,8 @@ function ProductsGrid({ products }: { products: Product[] }) {
         const { name, price, image } = product;
         const productId = product.id;
         const dollarsAmount = formatCurrency(price);
+        console.log(image);
+
         return (
           <article key={productId} className="group relative">
             <Link href={`/products/${productId}`}>
@@ -19,8 +21,9 @@ function ProductsGrid({ products }: { products: Product[] }) {
                 <CardContent className="p-4">
                   <div className="relative h-64 md:h-48 rounded overflow-hidden">
                     <Image
-                      src="https://euyapdgpjtdqhdgjdtkm.supabase.co/storage/v1/object/public/main-bucket/1734231097857-h-6.jpg"
-                      // src={image}
+                      // src="https://euyapdgpjtdqhdgjdtkm.supabase.co/storage/v1/object/public/main-bucket/1734231097857-h-6.jpg"
+
+                      src={`${image}`}
                       alt={name}
                       fill
                       sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw"
--- utils/actions.ts
+++ utils/actions.ts
@@ -21,7 +21,6 @@ const getAdminUser = async () => {
 };
 
 const renderError = (error: unknown): { message: string } => {
-  console.log(error);
   return {
     message: error instanceof Error ? error.message : 'An error occured!',
   };
