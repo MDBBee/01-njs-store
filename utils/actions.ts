fa3bc1f796eb1dfd4395ec3f9d5c6f9fa19e15e2
MDBBee <bobbyugbebor@gmail.com>
2024-12-17 17:09:02 +0200

    Add to cart in SP ✅

--- utils/actions.ts
+++ utils/actions.ts
@@ -441,7 +441,43 @@ const updateOrCreateCartItem = async ({
   }
 };
 
-export const updateCart = async (cart: Cart) => {};
+export const updateCart = async (cart: Cart) => {
+  // Retrieving all cart-items in the cart
+  const cartItems = await db.cartItem.findMany({
+    where: {
+      cartId: cart.id,
+    },
+    include: { product: true },
+  });
+
+  let numItemsInCart = 0;
+  let cartTotal = 0;
+
+  for (const item of cartItems) {
+    numItemsInCart += item.amount;
+    cartTotal += item.amount * item.product.price;
+  }
+
+  const tax = cart.taxRate * cartTotal;
+  const shipping = cartTotal ? cart.shipping : 0;
+  const orderTotal = cartTotal + tax + shipping;
+
+  const currentCart = await db.cart.update({
+    where: {
+      id: cart.id,
+    },
+    data: { numItemsInCart, cartTotal, tax, shipping, orderTotal },
+    include: {
+      cartItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  return currentCart;
+};
 
 export const addToCartAction = async (prevState: any, formData: FormData) => {
   const user = await getAuthUser();
